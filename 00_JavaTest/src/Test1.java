c8b9affa8834e0721fddaef2c072f193938a06ad
whgdkfk <jar3788@naver.com>
2025-01-20 11:25:37 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,32 @@
+// JS와 주석 동일
+
+/* Run 버튼 클릭 시 (단축키 ctrl + F11)
+ * 1) 자동으로 컴파일 수행(javac Test1.java) -> .class 파일(byte code) 생성
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class 파일명)
+ */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+			
+		System.out.println("이클립스 실행 중");
+		System.out.println("자꾸 그러지마...");
+		System.out.println("타자 연습 좀 해...");
+		System.out.println("으잉!");
+		
+		/* Java API DOC
+		 * : Java에서 제공해주는 코드의 사용법이 설명되어있는 문서
+		 * 
+		 * DOC 확인 방법(공부방법)
+		 * 1) google -> java 21 api 검색
+		 * 2) 궁금한 코드 클릭 -> shift + F2
+		 * 
+		 * [깃허브에 업로드X]
+		 * .metadata 폴더: 이클립스 설정 모음 폴더
+		 * .classpath: 클래스 경로 인식 방법 설정
+		 * bin 폴더: 컴파일된 파일(바이트 코드) 모인 폴더
+		 * .class 파일: 컴파일된 파일(바이트 코드)
+		 */
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,11 @@
+public class JavaTest{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("안녕하세요");
+		System.out.println("금요일...");
+
+		for(int i=1; i<=5; i++){
+			System.out.println(i + "번째 출력");
+		}
+	}
+}
